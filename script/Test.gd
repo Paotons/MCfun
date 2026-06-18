46eab7b06ebcc781a8e687414446c1fbe402928c
Paotons <3670587107@qq.com>
2026-06-18 16:35:00 +0800

    Fixmed titleraw.json

--- script/Test.gd
+++ script/Test.gd
@@ -4,6 +4,4 @@ extends EditorScript
 
 
 func _run() -> void:
-	for i : String in ["从前有座山", "山里有座庙", "庙里有个老和尚给小和尚讲故事", "讲的是什么呢"]:
-		var a := i.to_utf8_buffer()
-		print(Marshalls.raw_to_base64(a))
+	return
